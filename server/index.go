498cf6157ad62ba810a464759aa75e9f0e7ac466
chabare <chabare95@gmail.com>
2016-01-13 00:58:25 +0100

    Prepare for use of rows and columns

--- server/index.go
+++ server/index.go
@@ -1,11 +1,22 @@
 package server
 
 import (
-	"net"
 	"net/http"
 	"path/filepath"
+	"reflect"
 	"text/template"
 )
+
+// Columns ...
+type Columns struct {
+	Header []Header
+}
+
+// Row ...
+type Row struct {
+	Header  []Header
+	Columns []Columns
+}
 
 // IndexPage TODO: Comment
 type IndexPage struct {
@@ -17,7 +28,19 @@ type IndexPage struct {
 
 // IndexHandler TODO: Comment
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	funcMap := template.FuncMap{
+	t := template.Must(template.New("index.html").Funcs(createIndexFunctionMap()).ParseFiles(filepath.Join("resources/", "templates/", "index")+".html", "resources/templates/weather.tpl"))
+
+	loc := getLocation(r.RemoteAddr)
+
+	page := IndexPage{"Index", []string{"main", "header", "links"}, ReadLinkList(), loc}
+
+	t.Execute(w, page)
+}
+
+// NOTE: Use a file for function maps?!
+// createIndexFunctionMap TODO: Comment
+func createIndexFunctionMap() template.FuncMap {
+	return template.FuncMap{
 		"mod": func(i int, m int) int {
 			return i % m
 		},
@@ -34,12 +57,8 @@ func IndexHandler(w http.ResponseWriter, r *http.Request) {
 		"div": func(i, j int) int {
 			return i / j
 		},
+		"len": func(arr interface{}) int {
+			return reflect.ValueOf(arr).Len()
+		},
 	}
-	t := template.Must(template.New("index.html").Funcs(funcMap).ParseFiles(filepath.Join("resources/", "templates/", "index")+".html", "resources/templates/weather.tpl"))
-
-	loc := getLocation(net.ParseIP(r.RemoteAddr))
-
-	page := IndexPage{"Index", []string{"main", "header", "links"}, ReadLinkList(), loc}
-
-	t.Execute(w, page)
 }
